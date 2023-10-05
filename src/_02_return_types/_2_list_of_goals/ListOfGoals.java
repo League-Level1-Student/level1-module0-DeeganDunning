bdb36b769f3d79baee6e92682c1134ef1bb18176
student <student@192.168.1.98>
2023-10-05 13:47:37 -0700

    10-5-2023

--- src/_02_return_types/_2_list_of_goals/ListOfGoals.java
+++ src/_02_return_types/_2_list_of_goals/ListOfGoals.java
@@ -7,7 +7,7 @@ import javax.swing.JOptionPane;
 public class ListOfGoals {
 	static String listOfGoals ="";
 	static int numberOfGoals = 0;
-	
+
 	public static void main(String[] args) {
 		//1. Call the appropriate method to make your list of goals
 		createListOfGoals();
@@ -16,14 +16,17 @@ public class ListOfGoals {
 		JOptionPane.showMessageDialog(null, getListOfGoals());
 		//3. Call the appropriate method to get the number of goals you entered,
 		//   and using a pop-up display the number of goals
-		JOptionPane.showMessageDialog(null, "Number of goals is " + getNumberOfGoals());
+		JOptionPane.showMessageDialog(null, "You have " + getNumberOfGoals()+ " goals.");
 		//4. Call the appropriate method to see if you put a lot of goals,
 		//   if you did put a lot of goals use a pop-up to say "Wow that's a lot of goals! Awesome!"
 		if ( isManyGoals()) {
-			JOptionPane.showMessageDialog(null, ("Wow that's a lot of goals!"));
+			JOptionPane.showMessageDialog(null, ("Wow that's a lot of goals! Awesome!"));
 		}
+
+
 	}
-	
+
+
 	static void createListOfGoals() {
 		String input = "";
 		while(true) {
@@ -35,20 +38,20 @@ public class ListOfGoals {
 			numberOfGoals++;
 		}
 	}
-	
+
 	static String getListOfGoals() {
 		return listOfGoals;
 	}
-	
+
 	static int getNumberOfGoals() {
 		return numberOfGoals;
 	}
-	
+
 	static boolean isManyGoals() {
 		if( getNumberOfGoals() > 5) {
 			return true;
 		}
 		return false;
 	}
-	
+
 }
