239119de91b4c813a2480114435c24a09a1039a3
student <student@192.168.1.98>
2023-11-28 13:58:44 -0800

    11-28-2023

--- src/_04_gui_from_scratch/_4_cuteness_tv/VIdeoPlayer.java
+++ src/_04_gui_from_scratch/_4_cuteness_tv/VIdeoPlayer.java
@@ -1,5 +1,34 @@
 package _04_gui_from_scratch._4_cuteness_tv;
 
-public class VIdeoPlayer {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class VIdeoPlayer implements ActionListener {
+	JButton button1 = new JButton();
+	JButton button2 = new JButton();
+	JButton button3 = new JButton();
+
+	public void showButton(	) {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		frame.setVisible(true);
+		panel.add(button1);
+		panel.add(button2);
+		panel.add(button3);
+		frame.pack();
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		button3.addActionListener(this);
+}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
